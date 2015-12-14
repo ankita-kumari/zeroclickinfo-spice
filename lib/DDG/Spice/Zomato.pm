6a3bed58622baa2e814389419d0fe010ebbbf67c
dreamfyre <kumariankita002@gmail.com>
2015-12-14 00:42:19 +0000

    Added and modified template files

---
+++ lib/DDG/Spice/Zomato.pm
@@ -0,0 +1,28 @@
+package DDG::Spice::Zomato;
+
+# ABSTRACT: Information about restaurants, eateries and diners in India
+# Start at https://duck.co/duckduckhack/spice_overview if you are new
+# to instant answer development
+
+use DDG::Spice;
+
+# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
+spice is_cached => 1;
+spice proxy_cache_valid => "200 1d"; # defaults to this automatically
+
+spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
+
+# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
+spice to => 'https://developers.zomato.com/api/v2.1/search?entity_id=6&entity_type=city&count=10&api_key={{ENV{DDG_ZOMATO_API_KEY}}}&callback={{callback}}';
+
+# Triggers - https://duck.co/duckduckhack/spice_triggers
+triggers any => 'restaurants in hyderabad';
+
+# Handle statement
+handle remainder => sub {
+
+    # Query is in $_...if you need to do something with it before returning
+    return $_;
+};
+
+1;
